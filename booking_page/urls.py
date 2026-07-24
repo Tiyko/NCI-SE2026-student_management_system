8038995234855ba4cff8e048cc2686053542f1ef
Aidas04 <aidaskibas17@gmail.com>
2026-07-24 22:14:35 +0100

    Created Request Refund page - which works when signed in

--- booking_page/urls.py
+++ booking_page/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('<int:event_id>/', views.booking_page, name='booking_page'),
+    path("request_refund/", views.request_refund, name="request_refund"),
 ]
