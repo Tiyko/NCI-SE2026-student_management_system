4eefdc03f28cc1801e83e1c91af90b11fcc429c1
Aidas04 <aidaskibas17@gmail.com>
2026-08-01 20:21:05 +0100

    Added time awareness and used css styling for refund page

--- booking_page/urls.py
+++ booking_page/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path('<int:event_id>/', views.booking_page, name='booking_page'),
-    path("request_refund/", views.request_refund, name="request_refund"),
+    path('request_refund/', views.request_refund, name='request_refund'),
+    path('booking/remove/<int:booking_id>/', views.remove_booking, name='remove_booking'),
+    path('booking/cancel/<int:booking_id>/', views.cancel_booking, name='cancel_booking'),
 ]
